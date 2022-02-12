2c98f93bf8165f490b42b39bbe49aa425400a353
David Zapico <davidzapico@davidzapico.com>
2022-02-12 20:46:01 +0100

    12

--- asistente/citas-pendientes.php
+++ asistente/citas-pendientes.php
@@ -1,85 +1,77 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="../assets/css/style.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/table-style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="../assets/js/table-script.js"></script>
 </head>
+
 <body>
-	<?php
-	session_start();
-	$sessionErr = $consultaErr = "";
-    echo '<h3>Hola ' .$_SESSION["name"]. ' ' .$_SESSION["rol"]. '.</h3>';
-    echo '<a class="boton" href="logout.php">Cerrar sesión</a>';
-	if ($_SESSION["rol"] === "consultor") {
-		$_SESSION["con"] = mysqli_connect("localhost", "consultor", "consultor", "ventas");
-	} else {
-		$sessionErr = "No puedes consultar";
-	}
-	$hoy = date("Y-m-d");
-	?>
-		<div class="container">
-				<form action="#" method="post">
-					<p class="error"><?php ' . $sessionErr . ' ?></p>
-					<div>
-						Inicio: <input type="date" name="inicio" max="<?php echo $hoy ?>">
-						Fin: <input type="date" name="fin" max="<?php $hoy ?>">
-						<button type="submit" class="boton" name="consultar">Consultar</button>
-					</div>
-	<?php
-	if (isset($_POST['consultar'])) {
-		if (($_POST["inicio"] === "") && ($_POST["fin"] === "")) {
-			$inicio = "0000-00-00 00:00:00";
-			$fin =  $hoy . " 23:59:59";
-		} else if (($_POST["inicio"] === "") || ($_POST["fin"] === "")) {
-			$consultaErr = 'Introduce ambas fechas o deja ambas vacias.';
-		} else if ($_POST["inicio"] > $_POST["fin"]) {
-			$consultaErr = 'El "fin" debe ser posterior al "inicio".';
-		} else {
-			$inicio = $_POST["inicio"] . " 00:00:00";
-			$fin = $_POST["fin"] . " 23:59:59";
-		}
-		if ($consultaErr === "") {
-			$selectCompras = 'SELECT * FROM compras WHERE (fecha BETWEEN "' . $inicio . '" AND "' . $fin . '") AND (idusuario = "' . $_SESSION["idusuario"] . '")';
-			$consultarCompras = mysqli_query($_SESSION["con"], $selectCompras);
-			if (mysqli_num_rows($consultarCompras) === 0) {
-				$consultaErr = $_SESSION["name"] . ", no has realizado ninguna compra entre esas fechas.";
-			}
-		}
-		if ($consultaErr !== "") {
-			echo '<p class="error">' . $consultaErr . '</p>';
-		} else {
-			?>
-					<table>
-						<thead>
-							<th>Fecha</th>
-							<th>Nombre</th>
-							<th>Precio</th>
-							<th>Cantidad</th>
-						</thead>
-						<tbody>
-			<?php
-			while ($fetch = mysqli_fetch_array($consultarCompras)) {
-				$selectArticulos = 'SELECT descripcion FROM articulos WHERE idarticulo = "' . $fetch["idarticulo"] . '"';
-				$consultaNombreArticulo = mysqli_query($_SESSION["con"], $selectArticulos);
-				$nombreArticulo = mysqli_fetch_assoc($consultaNombreArticulo)["descripcion"];
-				echo '
-							<tr>
-								<td> ' . $fetch["fecha"] . '</td>
-								<td> ' . $nombreArticulo . '</td>
-								<td> ' . $fetch["precio_unitario"] . '€</td>
-								<td> ' . $fetch["cantidad"] . '</td>
-							</tr>
-				';
-			}
-			?>
-						</tbody>
-					</table>
-		<?php
-		}
-	}
-	?>
-				</form>
-		</div>
+    <?php
+    session_start();
+    if ($_SESSION['usutipo'] === 'Asistente') {
+        $_SESSION["con"] = mysqli_connect("localhost", "administrador", "", "Clinica");
+    } else {
+        header("location:../index.php");
+    }
+
+    $selectCitas = 'SELECT * FROM citas WHERE citEstado = "Asignado"';
+    $consultarCitas = mysqli_query($_SESSION["con"], $selectCitas);
+    if (mysqli_num_rows($consultarCitas) === 0) {
+        $consultaErr = $_SESSION["name"] . ", no has realizado ninguna compra entre esas fechas.";
+    }
+    ?>
+    <h1>Bienvenid@ <?php  '<p class="ufl">' . $_SESSION['name'] . ', bienvenid@! <br/>Por favor, espere mientras es redirigid@.</p>' ?></h1>
+    <button class="logout" data-hover="Bye 👋" onclick="window.location.href='../logout.php'">
+        <a href="../logout.php">
+            <div>Cerrar sesión</div>
+        </a>
+    </button>
+    <a href="index.php">Inicio</a>
+    <a href="alta-paciente.php">Alta paciente</a>
+    <a href="citas-atendidas.php">Citas atendidas</a>
+    <a href="citas-pendientes.php">Citas pendientes</a>
+    <a href="ver-pacientes.php">Ver pacientes</a>
+    <section>
+        <h1>Citas pendientes</h1>
+        <div class="tbl-header">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <thead>
+                    <tr>
+                        <th>ID Cita</th>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Paciente</th>
+                        <th>Medico</th>
+                        <th>Consultorio</th>
+                        <th>Observaciones</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
+        <div class="tbl-content">
+            <table cellpadding="0" cellspacing="0" border="0">
+                <tbody>
+                    <?php
+                    while ($fetch = mysqli_fetch_array($consultarCitas)) {
+                        echo '<tr>
+								<td> ' . $fetch["idCita"] . '</td>
+								<td> ' . $fetch["citFecha"] . '</td>
+								<td> ' . $fetch["citHora"] . '€</td>
+								<td> ' . $fetch["citPaciente"] . '</td>
+                                <td> ' . $fetch["citMedico"] . '</td>
+                                <td> ' . $fetch["citConsultorio"] . '</td>
+                                <td> ' . $fetch["CitObservaciones"] . '</td>
+							</tr>';
+                    }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
 </body>
+
 </html>
