a5ea101f12f4cc3fa23341d6392106fb4f17446e
David Zapico <davidzapico@davidzapico.com>
2022-03-06 19:11:41 +0100

    6

--- assets/sql/Privilegios.sql
+++ assets/sql/Privilegios.sql
@@ -11,9 +11,9 @@ GRANT SELECT, INSERT ON `Clinica`.`medicos` TO `Acceso`@`localhost`;
 
 GRANT USAGE ON *.* TO `Administrador`@`localhost` IDENTIFIED BY PASSWORD '*08852B3AFBFB593750CB9DB4F2E27E60258928AF';
 
-GRANT SELECT, INSERT ON `Clinica`.`usuarios` TO `Administrador`@`localhost`;
-
 GRANT SELECT, INSERT ON `Clinica`.`medicos` TO `Administrador`@`localhost`;
+
+GRANT SELECT, INSERT ON `Clinica`.`usuarios` TO `Administrador`@`localhost`;
 
 GRANT SELECT, INSERT ON `Clinica`.`pacientes` TO `Administrador`@`localhost`;
 
@@ -22,18 +22,24 @@ GRANT SELECT, INSERT ON `Clinica`.`pacientes` TO `Administrador`@`localhost`;
 
 GRANT USAGE ON *.* TO `Asistente`@`localhost` IDENTIFIED BY PASSWORD '*7D1BB3985F9CAC85224AAF18068F1C7FB6945700';
 
+GRANT SELECT, INSERT ON `Clinica`.`usuarios` TO `Asistente`@`localhost`;
+
 GRANT SELECT, INSERT ON `Clinica`.`citas` TO `Asistente`@`localhost`;
 
+GRANT SELECT ON `Clinica`.`consultorios` TO `Asistente`@`localhost`;
+
 GRANT SELECT, INSERT ON `Clinica`.`pacientes` TO `Asistente`@`localhost`;
+
+GRANT SELECT ON `Clinica`.`medicos` TO `Asistente`@`localhost`;
 
 
 # Privilegios para `Medico`@`localhost`
 
 GRANT USAGE ON *.* TO `Medico`@`localhost` IDENTIFIED BY PASSWORD '*93D6C5C072CA9F67C548FF2D832B217C41A938D0';
 
-GRANT SELECT ON `Clinica`.`medicos` TO `Medico`@`localhost`;
-
 GRANT SELECT ON `Clinica`.`pacientes` TO `Medico`@`localhost`;
+
+GRANT SELECT ON `Clinica`.`medicos` TO `Medico`@`localhost`;
 
 GRANT SELECT, INSERT, UPDATE ON `Clinica`.`citas` TO `Medico`@`localhost`;
 
